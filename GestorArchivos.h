24f31386e77a4e08b85d674686d08df166492169
alevibs <sarah.villota@correounivalle.edu.co>
2026-06-14 19:55:00 -0500

    Actualizacion GestorArchivos

---
+++ GestorArchivos.h
@@ -0,0 +1,23 @@
+#ifndef GESTORARCHIVOS_H
+#define GESTORARCHIVOS_H
+#include <string>
+#include "Tablero.h"
+#include "Vehiculo.h"
+#include "Parqueo.h"
+#include "Fila.h"
+
+class 
+{
+    public:
+    GestorArchivos();
+    ~GestorArchivos();
+
+    void CargarArchivos(std::string nombreArchivo);
+    Tablero getTablero();
+    Vehiculo getVehiculos();
+    Parqueo getParqueos();
+    Fila getFila();
+
+};
+
+#endif
