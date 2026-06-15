68cd4776fa19c5aa432ff7111108fdf818df3b77
alevibs <sarah.villota@correounivalle.edu.co>
2026-06-14 19:08:49 -0500

    Actualizacion Vehiculo

--- Vehiculo.h
+++ Vehiculo.h
@@ -1,1 +1,40 @@
-//holahola
+#ifndef VEHICULO_H
+#define VEHICULO_h
+
+#include <string>
+
+class Vehiculo
+{
+    private:
+    const std::string color;
+    int fila;
+    int columna;
+    int pasajerosAbordo;
+    const int largo;
+    const int capacidad;
+    char orientacion;
+
+    public:
+
+    Vehiculo(const std::string color,
+             int fila,
+             int columna,
+             int pasajerosAbordo,
+             const int largo,
+             const int capacidad,
+             char orientacion);
+    
+    std::string getColor();
+    int getFila();
+    int getColumna();
+    int getPasajerosAbordo();
+    int getLargo();
+    int getCapacidad();
+    char getOrientacion();
+
+    void SubirPasajero();
+    bool EstaLleno();
+
+};
+
+#endif
