d39b7db91291a73631c8efca07e604625af34469
alevibs <sarah.villota@correounivalle.edu.co>
2026-06-14 20:04:26 -0500

    Actualizacion VistaConsola

---
+++ VistaConsola.h
@@ -0,0 +1,21 @@
+#ifndef VISTACONSOLA_H
+#define VISTACONSOLA_H
+
+#include <string>
+
+class VistaConsola
+{
+    public:
+
+    void MostrarTablero();
+    void MostrarMenu();
+    void MostrarFila();
+    void MostrarParqueaderos();
+    void MostrarVictoria();
+    void MostrarMensaje(std:: string mensaje);
+    void SolicitarNivel();
+    void SolicitarMovimiento();
+    
+};
+
+#endif
